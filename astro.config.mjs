7beac07d27d862241e3e567cd7059fe3e971245a
olton <serhii@pimenov.com.ua>
2023-12-11 17:28:47 +0200

    added blog

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,13 @@
 import { defineConfig } from 'astro/config';
+import remarkToc from "remark-toc";
 
 export default defineConfig({
+    markdown: {
+        shikiConfig: {
+            theme: 'dracula',
+            langs: [],
+            wrap: false,
+        },
+        // remarkPlugins: [remarkToc]
+    },
 });
---
+++ src/js/utils.js
@@ -0,0 +1,31 @@
+export function slugify(text) {
+    return text
+        .toString()
+        .toLowerCase()
+        .replace(/\s+/g, '-')
+        .replace(/[^\w-]+/g, '')
+        .replace(/--+/g, '-')
+        .replace(/^-+/, '')
+        .replace(/-+$/, '')
+}
+
+export function formatDate (date) {
+    return new Date(date).toLocaleDateString('en-US')
+}
+
+export function buildToc(headings) {
+    if (!headings) return []
+    const toc = [];
+    const parentHeadings = new Map();
+    headings.forEach((h) => {
+        const heading = { ...h, subheadings: [] };
+        parentHeadings.set(heading.depth, heading);
+        // Change 2 to 1 if your markdown includes your <h1>
+        if (heading.depth === 2) {
+            toc.push(heading);
+        } else {
+            parentHeadings.get(heading.depth - 1) && parentHeadings.get(heading.depth - 1).subheadings.push(heading);
+        }
+    });
+    return toc;
+}

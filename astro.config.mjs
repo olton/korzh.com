67afa0d80be48f93092ba59619f08c16763bd447
olton <serhii@pimenov.com.ua>
2023-12-12 11:44:18 +0200

    added mdx support

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,16 @@
 import { defineConfig } from 'astro/config';
 import remarkToc from "remark-toc";
+import mdx from "@astrojs/mdx";
 
+// https://astro.build/config
 export default defineConfig({
-    markdown: {
-        shikiConfig: {
-            theme: 'dracula',
-            langs: [],
-            wrap: false,
-        },
-        // remarkPlugins: [remarkToc]
-    },
+  markdown: {
+    shikiConfig: {
+      theme: 'dracula',
+      langs: [],
+      wrap: false
+    }
+    // remarkPlugins: [remarkToc]
+  },
+  integrations: [mdx()]
 });

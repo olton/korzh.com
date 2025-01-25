0a162a1a0f6458cc8a91305141525f0607373d70
Serhii Pimenov <serhii@pimenov.com.ua>
2025-01-25 21:25:25 +0200

    add netlify adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,15 @@
 import { defineConfig } from 'astro/config';
 import mdx from "@astrojs/mdx";
 import { readingTime } from "./src/js/reading-time.js";
+import netlify from '@astrojs/netlify'
 
 import expressiveCode from "astro-expressive-code";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://korzh.com',
-  compressHTML: true,
+  compressHTML: true, 
+  adapter: netlify(),
   markdown: {
     // shikiConfig: {
     //   theme: 'dracula',

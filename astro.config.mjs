8c2b32a267eef7ea90714c9c5edb0eda87a5209c
Serhii Pimenov <serhii@pimenov.com.ua>
2025-01-25 21:43:55 +0200

    remove netlify adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,27 +1,21 @@
-import { defineConfig } from 'astro/config';
+import {defineConfig} from 'astro/config';
 import mdx from "@astrojs/mdx";
-import { readingTime } from "./src/js/reading-time.js";
+import {readingTime} from "./src/js/reading-time.js";
 import netlify from '@astrojs/netlify'
 
 import expressiveCode from "astro-expressive-code";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://korzh.com',
-  compressHTML: true, 
-  adapter: netlify(),
-  markdown: {
-    // shikiConfig: {
-    //   theme: 'dracula',
-    //   langs: [],
-    //   wrap: false
-    // },
-    remarkPlugins: [readingTime]
-  },
-  integrations: [
-      expressiveCode({
-        themes: ['dracula']
-      }),
-      mdx(),
-  ]
+    site: 'https://korzh.com',
+    compressHTML: true,
+    markdown: {
+        remarkPlugins: [readingTime]
+    },
+    integrations: [
+        expressiveCode({
+            themes: ['dracula']
+        }),
+        mdx(),
+    ]
 });
